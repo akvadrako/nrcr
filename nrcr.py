9b409f8b868b21a5fbe2513b437906a44608d89a
xinyue.zheng <xinyue.zheng@irdeto.com>
2016-09-15 18:39:57 +0200

    parse time

--- nrcr.py
+++ nrcr.py
@@ -1,9 +1,12 @@
 # http://doodle.com/poll/sz94cavn8r4657d2
-
 from selenium import webdriver
+import re
+from datetime import datetime
 
-# HTTP_PAGE = "http://doodle.com/poll/sz94cavn8r4657d2"
-HTTP_PAGE = "http://doodle.com/poll/45apuevsy5k9a3c28p7kk7km/admin"
+HTTP_PAGE = "http://doodle.com/poll/sz94cavn8r4657d2"
+#HTTP_PAGE = "http://doodle.com/poll/45apuevsy5k9a3c28p7kk7km/admin"
+prefered_time = datetime.strptime('10:20 AM', '%H:%M %p')
+min_delt = 0.0
 
 option_list = []
 
@@ -17,12 +20,17 @@ elem = browser.find_elements_by_css_selector('tr.participation td')
 # browser.find_elements_by_css_selector('tr.participation td:not(.disabled)')[1:][0].click()
 # browser.find_element_by_id('option0').is_selected()
 
-for event in browser.find_elements_by_css_selector('tr.participation td:not(.disabled)')[1:]:
+for event in browser.find_elements_by_css_selector('tr.participation td:not(.enabled)')[1:]:
     option_list.append(event)
 
 # browser.find_elements_by_css_selector('tr.participation td:not(.disabled)')[1:][0].get_attribute("title")
 for event in option_list:
-    print(event.get_attribute("title"))
+    option = (event.get_attribute("title"))
+    # regex = re.compile(r'\d+:\d+')
+    my_option = re.match(r"(.*?)(\d+:\d+ (A|P)M)", option)
+    date = datetime.strptime(my_option.group(2), '%H:%M %p')
+    delt= abs((date - prefered_time).total_seconds())
+    print(delt)
 
 print("pause")
 # elem.click()
