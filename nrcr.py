973444f4ab02ee0bf922d99e9831571a35383612
Devin Bayer <dev@doubly.so>
2016-09-15 16:47:29 +0200

    add clicking of button and sending email

--- nrcr.py
+++ nrcr.py
@@ -3,11 +3,12 @@
 from selenium import webdriver
 
 # HTTP_PAGE = "http://doodle.com/poll/sz94cavn8r4657d2"
-HTTP_PAGE = "http://doodle.com/poll/45apuevsy5k9a3c28p7kk7km/admin"
+# HTTP_PAGE = "http://doodle.com/poll/45apuevsy5k9a3c28p7kk7km/admin"
+HTTP_PAGE = "http://doodle.com/poll/yp4snzyx6cp3pwi5"
 
 option_list = []
 
-browser = webdriver.Firefox()
+browser = webdriver.PhantomJS()
 browser.get(HTTP_PAGE)
 
 # elem = browser.find_element_by_id('box0')
@@ -25,9 +26,36 @@ for event in option_list:
     print(event.get_attribute("title"))
 
 print("pause")
-# elem.click()
 
+name_field = browser.find_element_by_id('pname')
+name_field.send_keys('user')
+print name_field
 
+savebtn = browser.find_element_by_id('save')
+print savebtn
+print savebtn.click()
+print browser.current_url
+import time
+time.sleep(2)
+print browser.current_url
 
+from email.mime.text import MIMEText
+msg = MIMEText('''\
+    yaaa - massage
+    
+    {}
+
+'''.format(browser.current_url))
+msg['Subject'] = 'Massage'
+msg['From'] = 'dev@doubly.so'
+msg['To'] = 'dev@doubly.so'
+
+import smtplib
+s = smtplib.SMTP('nlhfd-msg001.domain1.intra')
+s.sendmail('dev@doubly.so', ['dev@doubly.so'], msg.as_string())
+s.quit()
+
+print msg.as_string()
+print 'email sent'
 
 
