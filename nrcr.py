79536845d7dda5693fb5ff213607eb3b2b86b95e
andrei.hutanu <andrei.hutanu@irdeto.com>
2016-09-15 16:41:19 +0200

    added config

--- nrcr.py
+++ nrcr.py
@@ -1,6 +1,16 @@
 # http://doodle.com/poll/sz94cavn8r4657d2
 
 from selenium import webdriver
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+username = config['default']['username']
+email = config['default']['email']
+prefered_time = config['default']['prefered_time']
+
+print(username, email, prefered_time)
 
 # HTTP_PAGE = "http://doodle.com/poll/sz94cavn8r4657d2"
 HTTP_PAGE = "http://doodle.com/poll/45apuevsy5k9a3c28p7kk7km/admin"
@@ -21,10 +31,13 @@ for event in browser.find_elements_by_css_selector('tr.participation td:not(.dis
     option_list.append(event)
 
 # browser.find_elements_by_css_selector('tr.participation td:not(.disabled)')[1:][0].get_attribute("title")
+
 for event in option_list:
     print(event.get_attribute("title"))
 
-print("pause")
+browser.quit()
+
+# print("pause")
 # elem.click()
 
 
